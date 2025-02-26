b0112d367e3f4dd96997fbbad9c7961b76bd8af9
João Rodrigo <joaofreire151208@gmail.com>
2025-02-26 19:23:46 -0300

    🎨 Criação da bolinha e das raquetes

--- script.js
+++ script.js
@@ -10,15 +10,30 @@ const canvasEL = document.querySelector("canvas")
         }
 
         function draw() {
+            // desenhando o campo
             canvasCTX.fillStyle = "#286047"
             canvasCTX.fillRect(0, 0, window.innerWidth, window.innerHeight) // Definir o tamanho do retângulo (x,y,largura,altura). Precisa nessa ordem: Largura e depois altura para não dar erro
             canvasCTX.fillStyle = '#ffff'
+
+            // linha central
             const x = window.innerWidth / 2 - linewidht / 2
             const y = 0
             const w = linewidht
             const h = window.innerHeight
 
             canvasCTX.fillRect(x, y, w, h)
+
+            // fazendo a raquete esquerda   
+            canvasCTX.fillRect(10, 400, linewidht, 200)
+
+            // raquete direita
+            canvasCTX.fillRect(window.innerWidth - linewidht - 10, 120, linewidht, 200)
+
+            // fazendo a bolinha
+            canvasCTX.beginPath()
+            canvasCTX.arc(200, 300, 20, 0, 2 * Math.PI, false)
+            canvasCTX.fill()
+
         }
 
 setup()
