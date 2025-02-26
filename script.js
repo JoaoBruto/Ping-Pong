123a9b483f687299fd5b5ff4b5a117c76cdb54d3
João Rodrigo <joaofreire151208@gmail.com>
2025-02-26 19:05:57 -0300

    🎨 Criação da linha central

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const canvasEL = document.querySelector("canvas")
         const canvasCTX = canvasEL.getContext("2d") // getContext, pega o contexto do
-
+        const linewidht = 15
 
         function setup() {
             canvasEL.width = window.innerWidth // Defini que a largura será do tamanho inteiro da janela
@@ -12,6 +12,13 @@ const canvasEL = document.querySelector("canvas")
         function draw() {
             canvasCTX.fillStyle = "#286047"
             canvasCTX.fillRect(0, 0, window.innerWidth, window.innerHeight) // Definir o tamanho do retângulo (x,y,largura,altura). Precisa nessa ordem: Largura e depois altura para não dar erro
+            canvasCTX.fillStyle = '#ffff'
+            const x = window.innerWidth / 2 - linewidht / 2
+            const y = 0
+            const w = linewidht
+            const h = window.innerHeight
+
+            canvasCTX.fillRect(x, y, w, h)
         }
 
 setup()
